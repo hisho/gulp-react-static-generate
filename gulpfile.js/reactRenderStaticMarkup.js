bf13947fafcbd944ac0aa692c6234d6f143163bf
hisho <hisho.web@gmail.com>
2021-05-12 20:21:57 +0900

    🔧 transpile typescript

--- gulpfile.js/reactRenderStaticMarkup.js
+++ gulpfile.js/reactRenderStaticMarkup.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const path = require('path');
+
+const tsProject = ts.createProject(path.resolve(process.cwd(),'tsconfig.json'));
 
 reactRenderStaticMarkup = (done) => {
   gulp
     .src('src/pages/**/*.tsx')
+    .pipe(tsProject())
     .pipe(gulp.dest('dist'))
   done();
 }
